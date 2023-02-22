3cdcbed7e2abe31cdeaad40a67ce9cc33a33bc80
Mahdi Razzaghi <103171417+MahdiRazzaghi808@users.noreply.github.com>
2023-02-22 04:54:16 +0330

    contact me

--- src/components/contact me/ContactMe.js
+++ src/components/contact me/ContactMe.js
@@ -2,15 +2,11 @@ import React from 'react'
 import styles from './ContactMe.module.css'
 import IconContent from './IconContent';
 
-
-// import { FaPhoneAlt } from "react-icons/fa";
-// import { FaRegEnvelope } from "react-icons/fa";
-
 const contentDetails = [
     { id: 1, title: 'تلگرام', icon: "telegram", href: "https://t.me/Mahdi_razzaghi808" },
     { id: 2, title: 'اینستاگرام', icon: "instagram", href: "https://instagram.com/mahdi_razzaghi0101?igshid=YmMyMTA2M2Y=" },
     { id: 3, title: 'گیتهاب', icon: "github", href: "https://github.com/MahdiRazzaghi808" },
-    { id: 4, title: 'لیندکدین', icon: "linkedin", href: "https://www.linkedin.com/in/mahdi-razzaghi-961965244" },
+    { id: 4, title: 'لینکدین', icon: "linkedin", href: "https://www.linkedin.com/in/mahdi-razzaghi-961965244" },
 ]
 
 const ContactMe = () => {
